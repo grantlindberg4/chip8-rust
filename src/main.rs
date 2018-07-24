d33557a92fbf5f77c946c8fdab72283c7176e89d
Grant Lindberg <grantlindberg4@gmail.com>
2018-07-24 14:36:18 -0700

    Load rom from command line

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 extern crate rand;
 extern crate sdl2;
 
+use std::env;
+
 mod cpu;
 mod core;
 
 fn main() {
     let mut cpu = cpu::Cpu::new();
     cpu.load_fontset();
-    match cpu.load_rom("./roms/pong2.c8") {
+
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2);
+    let rom = &args[1];
+    match cpu.load_rom(rom) {
         Ok(()) => {},
         Err(err) => { panic!("Error: {}", err) },
     }
+
     let sdl_context = sdl2::init().unwrap();
     match cpu.run(&sdl_context) {
         Ok(()) => {},
